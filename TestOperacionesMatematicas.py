70762c6fb0cc28db2542c63a0e4b35b0997325a5
lemi <lemi@lemi-HP-Pavilion-Laptop-15-cw1xxx.(none)>
2024-11-27 14:58:44 -0500

    correcciones

--- TestOperacionesMatematicas.py
+++ TestOperacionesMatematicas.py
@@ -1,25 +1,26 @@
 import unittest
+import operaciones_matematicas as op
 
 class TestOperacionesMatematicas(unittest.TestCase):
     def test_sumar(self):
-        self.assertEqual(sumar(3, 2), 5)
-        self.assertEqual(sumar(-1, 1), 0)
-        self.assertEqual(sumar(0, 0), 0)
+        self.assertEqual(op.sumar(3, 2), 5)
+        self.assertEqual(op.sumar(-1, 1), 0)
+        self.assertEqual(op.sumar(0, 0), 0)
     
     def test_restar(self):
-        self.assertEqual(restar(3, 2), 1)
-        self.assertEqual(restar(-1, 1), -2)
-        self.assertEqual(restar(0, 0), 0)
+        self.assertEqual(op.restar(3, 2), 1)
+        self.assertEqual(op.restar(-1, 1), -2)
+        self.assertEqual(op.restar(0, 0), 0)
     
     def test_multiplicar(self):
-        self.assertEqual(multiplicar(3, 2), 6)
-        self.assertEqual(multiplicar(-1, 1), -1)
-        self.assertEqual(multiplicar(0, 0), 0)
+        self.assertEqual(op.multiplicar(3, 2), 6)
+        self.assertEqual(op.multiplicar(-1, 1), -1)
+        self.assertEqual(op.multiplicar(0, 0), 0)
     
     def test_dividir(self):
-        self.assertEqual(dividir(6, 2), 3)
-        self.assertEqual(dividir(7, 3), 2.3333333333333335)
-        self.assertIsNone(dividir(3, 0))  
+        self.assertEqual(op.dividir(6, 2), 3)
+        self.assertEqual(op.dividir(7, 3), 2.3333333333333335)
+        self.assertIsNone(op.dividir(3, 0))  
 
 if __name__ == '__main__':
     unittest.main()
